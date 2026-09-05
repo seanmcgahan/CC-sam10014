bca7c2f756cd1d1c49637791a130c213ac67a44f
Sean McGahan <sean9mcgahan@gmail.com>
2026-09-05 12:28:16 -0400

    Update sketch.js
    2 more quotes added

--- week1/sketch.js
+++ week1/sketch.js
@@ -8,8 +8,8 @@ const quotes = [
   { text: "a new interdisciplinary art form that bridges the gap between technologists and artists", source: "Ahmad Moussa" },
   { text: "a method that uses computer programming for artistic expression. In this method, the goal is not predefined and the process is based on discovery, variation, and exploration of mostly unexpected results", source: "Tim Rodenbröcker" },
   { text: "a term we use to describe the computer programming software and people that make expressive art and design through code", source: "Maura Kelly" },
-  { text: "a community of practice", source: "Seth" },
-  { text: "an opportunity to stick it to the man", source: "Shifty-eyed Susan" }
+  { text: "the unexpected beauty of mathematical systems pushed to their limits", source: "wavgen.ca" },
+  { text: "the production of something that is expressive in nature rather than having a purely practical use", source: "Matt Crouch" }
 ];
 // no need to edit anything below this line! 
 // if you have made an error, you can check your history to see what might have gone wrong
