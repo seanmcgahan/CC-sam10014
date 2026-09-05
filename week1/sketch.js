32cc216717be9bb2c6ae8fd323f8780848ba7f63
Sean McGahan <sean9mcgahan@gmail.com>
2026-09-05 12:24:18 -0400

    Update sketch.js
    More quotes added

--- week1/sketch.js
+++ week1/sketch.js
@@ -3,11 +3,11 @@
 
 const quotes = [
   { text: "An approach to programming computers in which the primary goal is to create an aesthetic or expressive experience", source: "The College of New Jersey" },
-  { text: "a chance for code to be wrong", source: "Sara" },
-  { text: "surprise, exploration, and aesthetic exploration", source: "Sally" },
-  { text: "process over results", source: "Sam" },
-  { text: "form over function", source: "Sean" },
-  { text: "iterative and experimental", source: "Scooby" },
+  { text: "Programming without strict pre-planning", source: "Joachim Rodriguez y Romero" },
+  { text: "Programming with artistic purposes", source: "Mauricio Verano Merino, Juan Pablo Sáenz" },
+  { text: "a new interdisciplinary art form that bridges the gap between technologists and artists", source: "Ahmad Moussa" },
+  { text: "a method that uses computer programming for artistic expression. In this method, the goal is not predefined and the process is based on discovery, variation, and exploration of mostly unexpected results", source: "Tim Rodenbröcker" },
+  { text: "a term we use to describe the computer programming software and people that make expressive art and design through code", source: "Maura Kelly" },
   { text: "a community of practice", source: "Seth" },
   { text: "an opportunity to stick it to the man", source: "Shifty-eyed Susan" }
 ];
