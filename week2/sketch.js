8347b3a52fea390e3408843807130b47102b32e3
Sean McGahan <sean9mcgahan@gmail.com>
2026-09-11 15:58:56 -0400

    Update sketch.js
    Colors added

--- week2/sketch.js
+++ week2/sketch.js
@@ -8,15 +8,20 @@ function setup() {
 function draw() {
     background(210, 100, 100);
     strokeWeight(5);
+    stroke(300,70,100);
     triangle(50, 750, 50, 540, 260, 750);
+    push();
     strokeWeight(10);
+    stroke(90, 50, 100);
     line(0, 400, 400, 800);
-    strokeWeight(5);
+    pop();
     push();
     translate(400, 400);
     rotate(45);
+    stroke(0, 0, 100);
+    fill(300, 70, 100);
     rectMode(CENTER);
-    rect(0, 0, 300, 300);
+    rect(0, 0, 350, 350);
     pop();
     
 }
