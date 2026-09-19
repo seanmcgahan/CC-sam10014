eca7ea678a2af6d21bd7d7dc459c495f6898d204
Sean McGahan <sean9mcgahan@gmail.com>
2026-09-19 11:50:06 -0400

    Update sketch2.js
    Cleaned up code, fixed multiple mistakes, added some colors

--- week3/sketch2.js
+++ week3/sketch2.js
@@ -1,11 +1,10 @@
 
-let g = 40
-let h = 0
-let q = -40
+let g = 40;
+let h = 0;
 
 function setup() {
     createCanvas(800, 800);
-    colorMode(HSB)
+    colorMode(HSB);
 }
 
 function draw() {
@@ -19,7 +18,7 @@ function draw() {
 
     //if background 2
     if(mouseX > 50 && mouseX < 150 || mouseX > 250 && mouseX < 350 || mouseX > 450 && mouseX < 550 || mouseX > 650 && mouseX < 750) {
-        background (200, 25, 100)
+        background (200, 25, 100);
     }
 
     push()
@@ -65,34 +64,21 @@ function draw() {
 
         // color
         colorMode(HSB);
-        fill(360*(i*w/25), 100*(i*w/50), 100);
+        fill(360*(w*2/25), 100*(w*2/50) + 25, 100);
 
-        // if stroke
-        // strokeWeight(2)
-        // if(mouseY < 400) {
-        //     stroke(0, 0, 0);
-        // }
-        // else{
-        //     noStroke()
-        // }
 
-        rotate(0)
 
         //Draw Triangles
         push()
-        fill(0, 0, 100)
-        translate(-400 , -350)
-        triangle(i*200, w*75 + g, i*200 + g, w*75 + 0, i*200 - g, w*75 + 0)
+        fill(0, 0, 100);
+        translate(-400 , -350);
+        triangle(i*200, w*75 + g, i*200 + g, w*75 + 0, i*200 - g, w*75 + 0);
         pop()
 
         push()
-        translate(-300, -350)
-        triangle(-g + i*200, w*75 + 0, g + i*200, w*75 + 0, i*200 + 0, -g + w*75)
+        translate(-300, -350);
+        triangle(-g + i*200, w*75 + 0, g + i*200, w*75 + 0, i*200 + 0, -g + w*75);
         pop()
-        // Draw squares
-        //rectMode(CENTER);
-        //rect(i*100 - 300, w*100 - 300,
-            //i*w*1.2 + 50);
 
         }
     }
