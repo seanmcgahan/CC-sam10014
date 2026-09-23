1cefb07dab35ebd250c4f67a8a9350bcc6c4c444
Sean McGahan <sean9mcgahan@gmail.com>
2026-09-22 22:32:57 -0400

    Update sketch3.js
    Sketch 3 updated being saved (didnt realize these werent pushed to github yet)

--- week3/sketch3.js
+++ week3/sketch3.js
@@ -1,5 +1,4 @@
 
-let g = 40;
 
 function setup() {
     createCanvas(800, 800);
@@ -8,33 +7,28 @@ function setup() {
 
 function draw() {
 
-//    if background 1 (red background when outside of the canvas)
+//    if background
    if(mouseX < 800 && mouseX > 0 && mouseY > 0 && mouseY < 800) {
-        background(40, 50, 90);
+        background(0, 0, 100);
+        stroke (0, 0, 0)
    }else{
-        background(0, 25, 100);
+        background(0, 0, 0);
+        stroke(0, 0, 100)
    }
 
     //if background 2 (change background color for each vertical row across)
-    if(mouseX > 50 && mouseX < 150 || mouseX > 250 && mouseX < 350 || mouseX > 450 && mouseX < 550 || mouseX > 650 && mouseX < 750) {
-        background (200, 25, 100);
+    if(mouseY > 50 && mouseY < 150 || mouseY > 250 && mouseY < 350 || mouseY > 450 && mouseY < 550 || mouseY > 650 && mouseY < 750) {
+        background (0, 0, 0);
+        stroke(0, 0 , 100)
     }
 
-    // flip triangles when scrolling across
-    push()
-    if(mouseX < 0 || mouseX > 50 && mouseX < 150 || mouseX > 250 && mouseX < 350 || mouseX > 450 && mouseX < 550 || mouseX > 650 && mouseX < 750 || mouseX > 800) {
-        g = -40;
-    }else{
-        g = 40;
-    }
-    pop()
 
     
 
     // basic draw settings
     translate(400, 400);
     angleMode(DEGREES);
-    strokeWeight(2);
+    strokeWeight(3);
 
 
     for(let i = 0; i < 11; i++) { // for loop (x axis)
@@ -42,7 +36,7 @@ function draw() {
 
         // color
         colorMode(HSB);
-        fill(360*(w*2/25), 100*(w*2/50) + 25, 100);
+        fill(0, 0, w*50);
 
 
 
